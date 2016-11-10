87c835bbc0f4f596d561e3ccaaf865cd903a197d
FaycalElOuariachi <faycal.eloua@gmail.com>
2016-11-10 03:46:54 +0100

    Ajout du zoom | 'animation' du reset | Probleme concernant le centre de rotation

--- Assets/Scripts/Movement/RotateEarth.cs
+++ Assets/Scripts/Movement/RotateEarth.cs
@@ -32,7 +32,26 @@ public class RotateEarth : MonoBehaviour {
 	 * Direction courante du rayon allant du centre de la terre au point visé actuellement
 	 */
 	Vector3 currentDirection;
-
+	/**
+	 * Distance minimale de la caméra par rapport à la terre
+	 */
+	private float distanceMin = 10f;
+	/**
+	 * Distance maximale de la caméra par rapport à la terre
+	 */
+	private float distanceMax = 65f;
+	/**
+	 * Sensibilité du zoom de la  caméra
+	 */
+	private float sensitivity = 10f;
+	/**
+	 * Vitesse de déplacement de la terre lors du reset
+	 */
+	private float speedReset = 4f;
+	/**
+	 * Booléen permettant de bloquer toute action durant l'animation du reset
+	 */
+	private bool resetFlag = false;
 
 	// Use this for initialization
 	void Start () {
@@ -41,6 +60,22 @@ public class RotateEarth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		rotate ();
+		zoom();
+		tr.position = Vector3.zero;
+	}
+
+	/**
+	 * Rotation de la terre
+	 * ---
+	 * Suit le curseur, lorsque le clique droit de la souris est enfoncé
+	 */
+	private void rotate() {
+		if (resetFlag) {
+			resetRotation ();
+			return;
+		}
+
 		ray = camera.ScreenPointToRay (Input.mousePosition);
 		if (Physics.Raycast (ray, out hit)) {
 			if (startingDirection == Vector3.zero && Input.GetMouseButton(1)) {
@@ -48,7 +83,12 @@ public class RotateEarth : MonoBehaviour {
 			}
 			else if (Input.GetMouseButton(1)) {
 				currentDirection = hit.point;
+
+				// Axe de rotation
 				Vector3 rotationAxis = Vector3.Cross (startingDirection, currentDirection);
+				//rotationAxis.x = 0;
+				//rotationAxis.z = 0;
+
 				float rotationAngle = Vector3.Angle (startingDirection, currentDirection);
 				tr.RotateAround(earthCenter, rotationAxis, rotationAngle);
 				startingDirection = currentDirection;
@@ -59,8 +99,49 @@ public class RotateEarth : MonoBehaviour {
 		}
 	}
 
-	public void resetRotation() {
-		tr.rotation = Quaternion.identity;
-		tr.position = Vector3.zero;
+	public void resetRotationButton() {
+		resetFlag = true;
+		//tr.rotation = Quaternion.identity;
+		//tr.position = Vector3.zero;
+		//camera.orthographicSize = 65f;
+	}
+
+	private void resetRotation() {
+		float angleToTurn = Quaternion.Angle (tr.rotation, Quaternion.identity);
+		float turnSpeed = Mathf.Min (angleToTurn, speedReset);
+
+		Quaternion old = tr.rotation;
+
+		tr.rotation = Quaternion.Lerp (tr.rotation, Quaternion.identity, Mathf.Clamp01 (angleToTurn > 0 ? speedReset / angleToTurn : 0f));
+
+		if (camera.orthographicSize < 64f)
+			camera.orthographicSize = Mathf.Min (camera.orthographicSize + speedReset / 2f, 65f);
+		else
+			camera.orthographicSize = 65f;
+
+		if (tr.rotation == old && camera.orthographicSize == 65f)
+			resetFlag = false;
+	}
+
+	private void zoom() {
+		float wheel = Input.GetAxis ("Mouse ScrollWheel");
+		if (wheel < 0)
+			zoomIn (wheel);
+		else if (wheel > 0 )
+			zoomOut (wheel);
+	}
+
+	private void zoomIn(float zoom) {
+		if (camera.orthographicSize <= distanceMin)
+			return;
+
+		camera.orthographicSize += zoom * sensitivity;
+	}
+
+	private void zoomOut(float zoom) {
+		if (camera.orthographicSize >= distanceMax)
+			return;
+
+		camera.orthographicSize += zoom * sensitivity;
 	}
 }
---
+++ Assets/ZoomPays.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomPays : MonoBehaviour {
+
+	private MeshCollider collider;
+	private Vector3 boundsSize;
+	private float size;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<MeshCollider> ();
+		boundsSize = collider.bounds.size;
+		size = Mathf.Max (boundsSize.x, Mathf.Max (boundsSize.y, boundsSize.z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
