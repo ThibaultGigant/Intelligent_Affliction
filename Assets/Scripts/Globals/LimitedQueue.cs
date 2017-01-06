21187dd9f908e8af62b53a185296aec5e442757b
Thibault Gigant <thibault.gigant@gmail.com>
2017-01-06 14:18:16 +0100

    Ajout des commentaires pour la LimitedQueue

--- Assets/Scripts/Globals/LimitedQueue.cs
+++ Assets/Scripts/Globals/LimitedQueue.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+/**
+ * Classe permettant de faire des files de type FIFO de taille limitée dès la création
+ */
 public class LimitedQueue<T> : Queue<T>
 {
 
-
+	/**
+	 * Taille de la file
+	 */
 	public int size { get ; set ; }
 
+	/**
+	 * Constructeur
+	 */
 	public LimitedQueue (int size)
 	{
 		this.size = size;
 	}
 
+	/**
+	 * Permet d'ajouter un objet tout en gardant la file de la taille voulue
+	 */
 	public void Enqueue (T obj)
 	{
 		base.Enqueue (obj);
@@ -20,6 +31,9 @@ public class LimitedQueue<T> : Queue<T>
 		}
 	}
 
+	/**
+	 * Affichage de la file avec le type stocké et la taille de cette fille
+	 */
 	public string ToString ()
 	{
 		string str = "Limited Queue of type <" + typeof(T).Name + "> size : " + Count + "\n";
