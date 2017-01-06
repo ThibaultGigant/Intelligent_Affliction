84b7175a49fac7483649e688d548796a27632da5
Thibault Gigant <thibault.gigant@gmail.com>
2017-01-06 14:13:07 +0100

    Ajout LimitedQueue

---
+++ Assets/Scripts/Globals/LimitedQueue.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LimitedQueue<T> : Queue<T>
+{
+
+
+	public int size { get ; set ; }
+
+	public LimitedQueue (int size)
+	{
+		this.size = size;
+	}
+
+	public void Enqueue (T obj)
+	{
+		base.Enqueue (obj);
+		while (Count > size) {
+			base.Dequeue ();
+		}
+	}
+
+	public string ToString ()
+	{
+		string str = "Limited Queue of type <" + typeof(T).Name + "> size : " + Count + "\n";
+		int i = 0;
+		foreach (T t in this) {
+			str += i + " : " + t.ToString () + "\n";
+			i++;
+		}
+		str += "\n";
+		return str;
+	}
+}
